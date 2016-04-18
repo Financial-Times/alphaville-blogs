0f7685425980504e45b937f06de97c11e51299e9
Laurent Muchacho <laurent.muchacho@ft.com>
2016-04-18 15:02:39 +0100

    refactor of various part

--- routes/article.js
+++ routes/article.js
@@ -2,18 +2,11 @@
 
 var express = require('express');
 var router = express.Router();
-var elasticSearch = require('../lib/elasticSearchApi');
+var elasticSearch = require('alphaville-es-interface');
+var headerConfig = require('alphaville-header-config');
+
 var renderPage = require('../lib/renderPage');
 
-const headerConfig = require('../bower_components/alphaville-header/template_config.json');
-const envVars = require('../env');
-
-headerConfig.navItems.map(function (obj) {
-	if (obj.name.indexOf('The Blog')>-1) {
-		obj.selected = true;
-	}
-	return obj
-});
 
 /* GET article page. */
 router.get('/:uuid', (req, res) => {
@@ -23,7 +16,7 @@ router.get('/:uuid', (req, res) => {
 		renderPage(res, 'article', 'index',{
 			title: response._source.title + ' | FT Alphaville',
 			article : response._source,
-			headerConfig: headerConfig,
+			headerConfig: headerConfig.setSelected('The Blog'),
 			partials: {
 				twitterWidget: '../views/partials/twitterWidget.hjs',
 				postHeader: '../views/partials/postHeader.hjs'
--- routes/index.js
+++ routes/index.js
@@ -2,21 +2,10 @@
 
 var express = require('express');
 var router = express.Router();
-var elasticSearch = require('../lib/elasticSearchApi');
+var elasticSearch = require('alphaville-es-interface');
+var headerConfig = require('alphaville-header-config');
+
 var renderPage = require('../lib/renderPage');
-
-const headerConfig = require('../bower_components/alphaville-header/template_config.json');
-const envVars = require('../env');
-
-var limit = 30;
-
-headerConfig.navItems.map(function (obj) {
-	if (obj.name.indexOf('The Blog')>-1) {
-		obj.selected = true;
-	}
-	return obj
-});
-
 
 function isMarketLive(response) {
   response.hits.hits.map(function (obj) {
@@ -58,14 +47,14 @@ router.get('/', (req, res) => {
 				order : 'desc'
 			}
 		},
-		'size': limit || 30 })
+		'size': 30 })
 
 	}).then(isMarketLive).then(function(response){
 
 		renderPage(res, 'index', 'index',{
 			title: 'FT Alphaville | FT Alphaville &#8211; Market Commentary &#8211; FT.com',
 			searchResults : response.hits.hits,
-			headerConfig: headerConfig,
+			headerConfig: headerConfig.setSelected('The Blog'),
 			partials: {
 				twitterWidget: '../views/partials/twitterWidget.hjs',
 				postHeader: '../views/partials/postHeader.hjs'
