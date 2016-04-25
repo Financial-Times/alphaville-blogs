bd6359574100cd82ad3c80ba8b262644a42ed6bf
Dragos Digulescu <dragos.digulescu@iquestgroup.com>
2016-04-25 11:48:55 +0300

    Add barrier and alphaville-auth-middleware

--- routes/article.js
+++ routes/article.js
@@ -4,9 +4,29 @@ var express = require('express');
 var router = express.Router();
 var elasticSearch = require('alphaville-es-interface');
 var headerConfig = require('alphaville-header-config');
+var auth = require('alphaville-auth-middleware');
+var env = process.env['ENVIRONMENT'] || 'test';
 
 var renderPage = require('alphaville-page-render');
 
+var authConfig = {
+	checkHeader: process.env['AUTH_HEADER'],
+	barrierView: 'barrier',
+	viewModel: {
+		headerConfig: headerConfig.setSelected('The Blog'),
+		assetsBasePath: '/assets/index',
+		basePath: '/index',
+		isTest: env === 'test' ? true : false,
+		isProd: env === 'prod' ? true : false,
+		partials: {
+			header: '../bower_components/alphaville-header/main.hjs',
+			footer: '../bower_components/alphaville-footer/main.hjs',
+			body: '../bower_components/alphaville-barrier/main.hjs'
+		}
+	}
+};
+
+router.use('/', auth(authConfig));
 
 /* GET article page. */
 router.get('/:uuid', (req, res) => {
