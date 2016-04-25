b8c03a6b5f7a35ff7804ccc5595c780a59ef5ac1
Dragos Digulescu <dragos.digulescu@iquestgroup.com>
2016-04-25 10:30:30 +0300

    Add access meta for articles and marketslive

--- routes/index.js
+++ routes/index.js
@@ -67,6 +67,14 @@ router.get('/__access_metadata', (req, res) => {
 	res.json({
 			access_metadata: [
 				{
+					path_regex: "/content/(?<uid>[a-f0-9\-]+)",
+					classification: "conditional_registered"
+				},
+				{
+					path_regex: "/marketslive/(?<uid>[a-f0-9\-]+)",
+					classification: "conditional_registered"
+				},
+				{
 					path_regex: "/longroom",
 					classification: "conditional_registered"
 				},
