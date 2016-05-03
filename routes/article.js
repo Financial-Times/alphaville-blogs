4c56b522bd7ea47c01f760e4ea04fa9de506dd47
Dragos Digulescu <dragos.digulescu@iquestgroup.com>
2016-05-03 21:31:12 +0300

    Update to use latest version of auth middleware and alphaville-espress

--- routes/article.js
+++ routes/article.js
@@ -8,27 +8,10 @@ const elasticSearch = require('alphaville-es-interface');
 const auth = require('alphaville-auth-middleware');
 
 const externalPartials = {
-	barrier: fs.readFileSync(path.join(__dirname, '../bower_components/alphaville-barrier/main.handlebars'), 'utf-8'),
 	commentsConfig: fs.readFileSync(path.join(__dirname, '../node_modules/alphaville-comments-config/main.handlebars'), 'utf-8')
 };
 
-const authConfig = {
-	checkHeader: process.env['AUTH_HEADER'],
-	checkHeaderValue: process.env['AUTH_HEADER_VALUE']
-};
-
-router.use('/', auth(authConfig), (req, res, next) => {
-	if (req.hasOwnProperty('isAuthenticated') && req.isAuthenticated === false ) {
-		return res.render('barrier', {
-			title: 'FT Alphaville',
-			barrierModel: req.barrierModel,
-			partials: {
-				barrier: externalPartials.barrier
-			}
-		});
-	}
-	return next();
-});
+router.use('/', auth());
 
 /* GET article page. */
 router.get('/:uuid', (req, res) => {
