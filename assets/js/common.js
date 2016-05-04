1658465f515e91be7749f06e82af5df2a231b17f
Roland Vachter <roland.vachter@iquestgroup.com>
2016-05-04 10:43:55 +0300

    Fix assets paths.

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,4 +1,1 @@
-const assetsDomain = require('../../build_config/js/assetsDomain');
-const fingerprint = require('../../build_config/js/fingerprint');
-
-require('o-assets').setGlobalPathPrefix(assetsDomain + '/assets/index/bower/' + fingerprint + '/');
+require('./assets');
