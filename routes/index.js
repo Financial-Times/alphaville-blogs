c44fadd2e6320580702df3b9ff92feac17689e2d
Laurent Muchacho <laurent.muchacho@ft.com>
2016-05-25 17:58:01 +0100

    started card styling following card design brief document

--- routes/index.js
+++ routes/index.js
@@ -19,16 +19,43 @@ function getMetadata(metadata, options) {
 
 function categorization(response) {
 	response.hits.hits.forEach(function(obj) {
-		if (obj._source.title.indexOf('Markets Live:') > -1) {
+		var title = obj._source.title;
+		if (title.indexOf('Markets Live:') > -1) {
 			obj._webUrl = '/marketslive/' + obj._id;
 			obj.isMarketLive = true;
+			obj._source.primaryTheme = 'Market Live';
+			obj._source.title = obj._source.title.replace(/Markets Live: /, '');
+
 		} else {
 			obj._webUrl = '/content/' + obj._id;
 			obj.isMarketLive = false;
+			obj._source.primaryTheme = false;
+			if (getMetadata(obj._source.metadata, {prefLabel:'First FT'}).length > 0) {
+				obj._source.primaryTheme = 'BRIEFING: First FT';
+			}
+			// obj._source.openingHTML = obj._source.openingHTML.substring(0, 119);
+
 		}
-		obj.isPodcast = (getMetadata(obj._source.metadata, {prefLabel:'Podcasts'}).length > 0)
+		obj.isPodcast = (getMetadata(obj._source.metadata, {prefLabel:'Podcasts'}).length > 0);
+		if (obj.isPodcast) {
+			obj._source.primaryTheme = 'Podcast: Alphachat';
+		}
+
+		if(obj._source.title.length > 120){
+			obj._source.title = obj._source.title.substring(0, 118) + '&hellip;';
+		}
+
+
+
 	});
 	return response;
+}
+
+function testCat(response) {
+	response.hits.hits.forEach(function (obj) {
+	  console.log('primaryTheme: ', obj._source.primaryTheme);
+	});
+  return response;
 }
 
 
@@ -39,14 +66,23 @@ router.get('/', (req, res) => {
 		'method': 'POST',
 		'body': JSON.stringify({
 			'filter': {
-				and: {
+				or: {
 					filters: [{
 						term: {
 							"metadata.primary": {
 								value: "brand"
 							},
 							"metadata.idV1": {
-								value: "ZDkyYTVhMzYtYjAyOS00OWI1LWI5ZTgtM2QyYTIzYjk4Y2Jj-QnJhbmRz"
+								value: "ZDkyYTVhMzYtYjAyOS00OWI1LWI5ZTgtM2QyYTIzYjk4Y2Jj-QnJhbmRz" // FT Alphaville
+							}
+						}
+					},{
+						term: {
+							"metadata.primary": {
+								value: "brand"
+							},
+							"metadata.idV1": {
+								value: "N2NkMjJiYzQtOGI3MC00NTM4LTgzYmYtMTQ3YmJkZGZkODJj-QnJhbmRz" // First FT
 							}
 						}
 					}]
@@ -60,7 +96,7 @@ router.get('/', (req, res) => {
 			'size': 30
 		})
 
-	}).then(categorization).then(function(response) {
+	}).then(categorization).then(testCat).then(function(response) {
 
 		// res.jsonp(response.hits.hits);
 
