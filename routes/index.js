6ce0605d7e6c096a6791f15b359e734d16f2f4fa
Laurent Muchacho <laurent.muchacho@ft.com>
2016-05-27 17:48:13 +0100

    homepage card design

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,10 @@ const elasticSearch = require('alphaville-es-interface');
 
 const externalPartials = {
 	commentsConfig: fs.readFileSync(path.join(__dirname, '../node_modules/alphaville-comments-config/main.handlebars'), 'utf-8'),
+	card_blog : fs.readFileSync(path.join(__dirname, '../views/partials/card-blog.handlebars'), 'utf-8'),
+	card_briefing : fs.readFileSync(path.join(__dirname, '../views/partials/card-briefing.handlebars'), 'utf-8'),
 	card_hero : fs.readFileSync(path.join(__dirname, '../views/partials/card-hero.handlebars'), 'utf-8'),
+	card_marketlive : fs.readFileSync(path.join(__dirname, '../views/partials/card-marketlive.handlebars'), 'utf-8'),
 	card_podcast : fs.readFileSync(path.join(__dirname, '../views/partials/card-podcast.handlebars'), 'utf-8')
 };
 
@@ -18,44 +21,64 @@ function getMetadata(metadata, options) {
 	});
 }
 
-function ellipsis(str, l){
+function ellipsisTrim(str, l){
 	var len = l-1;
 	return (str.length > len)? str.substring(0, len) + '&hellip;' : str;
 }
 
+var isHeroSelected = false;
 function categorization(response) {
 	response.hits.hits.forEach(function(obj) {
-		var title = obj._source.title;
-		if (title.indexOf('Markets Live:') > -1) {
+
+		obj._source.standout = {
+			hero : false,
+			authorLead : false,
+			topicLead : false,
+			authorLeadImage : false,
+			image : false
+		}
+
+		if (obj._source.title.indexOf('Markets Live:') > -1) {
+			obj.isMarketLive = true;
 			obj._webUrl = '/marketslive/' + obj._id;
 			obj.isMarketLive = true;
 			obj._source.primaryTheme = 'Market Live';
 			obj._source.title = obj._source.title.replace(/Markets Live: /, '');
-			obj._source.cardType = 'marketLive';
+			obj._source.cardType = 'marketlive';
 
 		} else {
 			obj._webUrl = '/content/' + obj._id;
-			obj.isMarketLive = false;
 			obj._source.primaryTheme = false;
-			obj._source.cardType = '';
+			obj._source.cardType = 'blogs';
 
 			if (getMetadata(obj._source.metadata, {prefLabel:'First FT'}).length > 0) {
+				obj.isBriefing = true;
 				obj._source.primaryTheme = 'BRIEFING: First FT';
 				obj._source.cardType = 'firstFt';
+				obj._source.title = ellipsisTrim(obj._source.title, 240);
+				obj._source.summaries = ['Apple executive eyes media business', 'At Goldman, you’re more than a number', 'Universal basic income: money for nothing']
+
+			} else if (getMetadata(obj._source.metadata, {prefLabel:'Podcasts'}).length > 0) {
+				obj.isPodcast = true;
+				obj._source.primaryTheme = 'Podcast: Alphachat';
+				obj._source.cardType = 'podcast';
+				obj._source.title = ellipsisTrim(obj._source.title, 60);
+
+			} else {
+				obj.isBlog = true;
+				if (!isHeroSelected && obj._source.title.indexOf('Alphachat:') === -1 && obj._source.title.indexOf('Further reading') === -1) {
+					obj._source.standout.hero = true;
+					obj._source.primaryTheme = 'Markets';
+					isHeroSelected = true;
+				}
+
+			}
+
+			if(obj._source.title.length > 120){
+				obj._source.title = ellipsisTrim(obj._source.title, 120);
 			}
 
 		}
-		obj.isPodcast = (getMetadata(obj._source.metadata, {prefLabel:'Podcasts'}).length > 0);
-		if (obj.isPodcast) {
-			obj._source.primaryTheme = 'Podcast: Alphachat';
-			obj._source.cardType = 'podcast';
-			obj._source.title = ellipsis(obj._source.title, 60);
-		}
-
-		if(obj._source.title.length > 120){
-			obj._source.title = ellipsis(obj._source.title, 120);
-		}
-
 	});
 	return response;
 }
@@ -107,7 +130,10 @@ router.get('/', (req, res) => {
 			res.set('Cache-Control', 'public, max-age=30');
 		}
 
-		var hero = response.hits.hits.shift();
+		var heroIndex = response.hits.hits.findIndex(function (item) {
+			return item._source.standout.hero;
+		})
+		var hero = response.hits.hits.splice(heroIndex, 1);
 
 		res.render('index', {
 			title: 'FT Alphaville | FT Alphaville &#8211; Market Commentary &#8211; FT.com',
@@ -115,7 +141,10 @@ router.get('/', (req, res) => {
 			hero : hero,
 			partials: {
 				commentsConfig: externalPartials.commentsConfig,
+				card_blog : externalPartials.card_blog,
+				card_briefing : externalPartials.card_briefing,
 				card_hero: externalPartials.card_hero,
+				card_marketlive : externalPartials.card_marketlive,
 				card_podcast: externalPartials.card_podcast
 			}
 		});
