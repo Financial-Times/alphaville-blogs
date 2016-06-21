8ffff413a9bd7ad8481e6cb9ca81d17fb2bfb8c9
Dragos Digulescu <dragos.digulescu@iquestgroup.com>
2016-06-21 17:10:19 +0300

    Update __access_metadata to have more specific rules first

--- routes/index.js
+++ routes/index.js
@@ -286,11 +286,11 @@ router.get('/__access_metadata', (req, res) => {
 			path_regex: "/marketslive/(?<uid>[a-f0-9\-]+)",
 			classification: "conditional_registered"
 		}, {
-			path_regex: "/longroom/.*",
-			classification: "conditional_registered"
-		}, {
 			path_regex: "/longroom/content/.*",
 			classification: "conditional_alphaville_longroom"
+		}, {
+			path_regex: "/longroom/.*",
+			classification: "conditional_registered"
 		}, {
 			path_regex: ".*",
 			classification: "unconditional"
