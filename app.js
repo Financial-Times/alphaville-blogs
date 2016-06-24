de383a88b880f8d009fb68bed21cabe3a83274b6
Roland Vachter <roland.vachter@iquestgroup.com>
2016-06-24 16:13:10 +0300

    Correct error handling.

--- app.js
+++ app.js
@@ -33,24 +33,32 @@ app.use(function(req, res, next) {
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
+if (app.get('env') === 'development' || process.env.ENVIRONMENT !== 'prod') {
 	app.use(function(err, req, res, next) {
-		res.status(err.status || 500);
-		res.render('error', {
-			message: err.errMsg || err.message,
-			error: err
-		});
+		if (err.status === 404) {
+			res.sendStatus(404);
+		} else {
+			res.status(err.status || 503);
+			res.render('error', {
+				message: err.errMsg || err.message,
+				error: err
+			});
+		}
 	});
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-	res.status(err.status || 500);
-	res.render('error', {
-		message: err.errMsg || err.message,
-		error: {}
-	});
+	if (err.status === 404) {
+		res.sendStatus(404);
+	} else {
+		res.status(err.status || 503);
+		res.render('error', {
+			message: err.errMsg || err.message,
+			error: {}
+		});
+	}
 });
 
 module.exports = app;
