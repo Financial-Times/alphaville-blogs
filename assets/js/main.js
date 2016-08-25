25678a99a020add61621108323574e57ef4f376e
Roland Vachter <roland.vachter@iquestgroup.com>
2016-08-25 17:15:11 +0300

    Infinite scroll added to the homepage.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,18 @@
 require('./common');
 require('o-comments');
 require('o-comment-count');
+
+const oDate = require('o-date');
+const InfiniteScroll = require('alphaville-ui').InfiniteScroll;
+
+document.addEventListener('o.DOMContentLoaded', function () {
+	new InfiniteScroll({
+		pageParamName: 'page',
+		container: '.alphaville-infinite-scroll-container',
+		onNewPage: () => {
+			oDate.init();
+		}
+	});
+});
+
 require('o-autoinit');
