3f570e24df8b7251abb264044def097b0d507f39
Roland Vachter <roland.vachter@iquestgroup.com>
2016-09-07 13:58:30 +0300

    AV-667: Expanding Articles in List View

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,17 @@
 require('./common');
 require('o-comments');
+require('o-expander');
+require('o-video');
+require('o-share');
+
+const embeddedMedia = require('webchat/src/js/ui/embeddedMedia');
 const oCommentCount = require('o-comment-count');
 
 const oDate = require('o-date');
-const InfiniteScroll = require('alphaville-ui').InfiniteScroll;
+const alphavilleUi = require('alphaville-ui');
+const InfiniteScroll = alphavilleUi.InfiniteScroll;
+
+
 
 document.addEventListener('o.DOMContentLoaded', function () {
 	if (document.querySelector('.alphaville-infinite-scroll-container')) {
@@ -16,6 +24,34 @@ document.addEventListener('o.DOMContentLoaded', function () {
 			}
 		});
 	}
+
+	document.addEventListener('oExpander.expand', (evt) => {
+		const content = evt.srcElement.querySelector('.o-expander__content');
+		if (!content.innerHTML) {
+			content.innerHTML = `
+				<div class="alphaville-spinner"></div>
+			`;
+			alphavilleUi.utils.httpRequest.get({
+				url: evt.srcElement.getAttribute('data-article-url'),
+				query: {
+					ajax: true
+				}
+			}).then((html) => {
+				content.innerHTML = html;
+
+				const closedContentContainer = content.querySelector('.webchat-closed-content');
+
+				if (closedContentContainer) {
+					embeddedMedia.convert(closedContentContainer);
+				}
+			}).catch((e) => {
+				console.log(e);
+			});
+		}
+	});
+
+	document.addEventListener('oExpander.collapse', () => {
+	});
 });
 
 require('o-autoinit');
