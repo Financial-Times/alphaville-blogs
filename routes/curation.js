aedb72b89eac2d0f07c48098d3a2a797273d977b
Roland Vachter <roland.vachter@iquestgroup.com>
2016-09-22 12:15:23 +0300

    Fix ML vanity URL regex.

--- routes/curation.js
+++ routes/curation.js
@@ -4,7 +4,7 @@ const express = require('express');
 const router = new express.Router();
 const curationCtrl = require('../lib/controllers/curationCtrl');
 const auth = require('alphaville-auth-middleware');
-const restrictedAccess = require('./lib/middlewares/restrictedAccess');
+const restrictedAccess = require('../lib/middlewares/restrictedAccess');
 
 router.get('/', auth(), restrictedAccess, curationCtrl.index);
 
