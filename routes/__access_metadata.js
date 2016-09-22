0d9b6eb2869d8d6c425ee96875bedaded8e3acaf
Roland Vachter <roland.vachter@iquestgroup.com>
2016-09-22 11:25:13 +0300

    Add vanity URLs of articles and marketslive, and the curation tool.

--- routes/__access_metadata.js
+++ routes/__access_metadata.js
@@ -6,7 +6,19 @@ router.get('/__access_metadata', (req, res) => {
 			path_regex: "/content/(?<uid>[a-f0-9\-]+)",
 			classification: "conditional_registered"
 		}, {
+			path_regex: "/(?[0-9]+)/(?[0-9]+)/(?[0-9]+)/(?[0-9]+)/.*",
+			classification: "conditional_registered"
+		}, {
+			path_regex: "/marketslive",
+			classification: "unconditional"
+		}, {
 			path_regex: "/marketslive/(?<uid>[a-f0-9\-]+)",
+			classification: "conditional_registered"
+		}, {
+			path_regex: "/marketslive/([0-9]+\-[0-9]+\-[0-9]+-?[0-9]+?\/?)",
+			classification: "conditional_registered"
+		}, {
+			path_regex: "/curation/.*",
 			classification: "conditional_registered"
 		}, {
 			path_regex: "/longroom/content/.*",
