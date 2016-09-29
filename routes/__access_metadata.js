a9ec2e4a5c6e73991c3021293b9aea442df45516
Dragos Digulescu <dragos.digulescu@iquestgroup.com>
2016-09-29 18:52:31 +0300

    Temporary remove most of the rules to debug access_metadata

--- routes/__access_metadata.js
+++ routes/__access_metadata.js
@@ -3,24 +3,6 @@ const router = require('express').Router();
 router.get('/__access_metadata', (req, res) => {
 	res.json({
 		access_metadata: [{
-			path_regex: "/content/(?<uid>[a-f0-9\-]+)",
-			classification: "conditional_registered"
-		}, {
-			path_regex: "/(?[0-9]+)/(?[0-9]+)/(?[0-9]+)/(?[0-9]+)/.*",
-			classification: "conditional_registered"
-		}, {
-			path_regex: "/marketslive",
-			classification: "unconditional"
-		}, {
-			path_regex: "/marketslive/(?<uid>[a-f0-9\-]+)",
-			classification: "conditional_registered"
-		}, {
-			path_regex: "/marketslive/([0-9]+)-([0-9]+)-([0-9]+)(?-)(?[0-9]+).*",
-			classification: "conditional_registered"
-		}, {
-			path_regex: "/curation/.*",
-			classification: "conditional_registered"
-		}, {
 			path_regex: "/longroom/content/.*",
 			classification: "conditional_alphaville_longroom"
 		}, {
