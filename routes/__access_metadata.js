d33700ea3a1f870e42d343a4fda80752005deac7
Dragos Digulescu <dragos.digulescu@iquestgroup.com>
2016-09-30 15:30:19 +0300

    Add access metadata for curation tool

--- routes/__access_metadata.js
+++ routes/__access_metadata.js
@@ -3,6 +3,9 @@ const router = require('express').Router();
 router.get('/__access_metadata', (req, res) => {
 	res.json({
 		access_metadata: [{
+			path_regex: "/curation/.*",
+			classification: "conditional_registered"
+		}, {
 			path_regex: "/marketslive/\\d{4}-\\d{2}-\\d{2}.*",
 			classification: "conditional_registered"
 		}, {
