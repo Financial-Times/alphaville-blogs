376a3b7f5854c4e5b0660c7138a5b8ad25300340
Roland Vachter <roland.vachter@iquestgroup.com>
2016-10-21 13:22:51 +0300

    Add redirection from meet the team dedicated pages to the generic page, with scrolling to the editor's section.

--- routes/meet.js
+++ routes/meet.js
@@ -3,6 +3,7 @@
 const router = require('express').Router();
 const meetCtrl = require('../lib/controllers/meetCtrl');
 
-router.get('/', meetCtrl);
+router.get('/', meetCtrl.index);
+router.get('/:member', meetCtrl.dedicatedPage);
 
 module.exports = router;
