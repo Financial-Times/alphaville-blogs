15e2a483627074a07a64669b5c768a817e3271df
Roland Vachter <roland.vachter@iquestgroup.com>
2016-10-26 13:16:54 +0300

    AV-928: During active ML session the ML index card is not styled appropriately

--- assets/js/ml-live.js
+++ assets/js/ml-live.js
@@ -2,10 +2,29 @@ require('./common');
 require('alphaville-marketslive-chat');
 require('./ml-pageBackground');
 require('o-tabs');
+const Scroller = require('./utils/Scroller');
 
 exports.Webchat = require('webchat');
 
 exports.marketsLiveSessionNotification = require('alphaville-ui')['marketslive-session-notification'];
 exports.marketsLiveSessionNotification.disableNotification();
+
+document.addEventListener('o.DOMContentLoaded', () => {
+	const mlTabsContainer = document.querySelector('.ml-tabs-container');
+	const stickyHeader = document.querySelector('.alphaville-header--sticky');
+
+	const scroller = new Scroller(document.body, () => {
+		if (stickyHeader.classList.contains('o-header--sticky-active') && !mlTabsContainer.classList.contains('ml-tabs-container--sticky')) {
+			mlTabsContainer.classList.add('ml-tabs-container--sticky');
+			mlTabsContainer.style.top = stickyHeader.clientHeight + 'px';
+		}
+
+		if (!stickyHeader.classList.contains('o-header--sticky-active') && mlTabsContainer.classList.contains('ml-tabs-container--sticky')) {
+			mlTabsContainer.classList.remove('ml-tabs-container--sticky');
+			mlTabsContainer.style.top = 'auto';
+		}
+	});
+	scroller.start();
+});
 
 require('o-autoinit');
