e402c6f46f53629390b7fd48b014584cdd8334cc
Roland Vachter <roland.vachter@iquestgroup.com>
2016-11-04 15:53:03 +0200

    Use the newly created about page.

--- routes/about.js
+++ routes/about.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const router = require('express').Router();
-const aboutCtrl = require('../lib/controllers/wpStaticCtrl')('about', 'about-alphaville-2');
+const aboutCtrl = require('../lib/controllers/wpStaticCtrl')('about', 'about-ft-alphaville');
 
 router.use('/', aboutCtrl);
 
