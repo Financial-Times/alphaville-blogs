19d96ed8814007a9df5dec654f3a6cdacf698fbb
Roland Vachter <roland.vachter@iquestgroup.com>
2016-11-09 10:47:56 +0200

    Increase image quality for cards.

--- views/helpers/image.js
+++ views/helpers/image.js
@@ -4,8 +4,10 @@ const url = require('url');
 const _ = require('lodash');
 
 module.exports = (imgUrl, width, quality) => {
+	console.log(imgUrl, 'quality 1', quality);
 	width = parseInt(width, 10) || 250;
-	quality = Handlebars.escapeExpression(quality) || 'low';
+	quality = Handlebars.escapeExpression(quality) || 'medium';
+	console.log(imgUrl, 'quality 2', quality);
 	const parsedImgUrl = url.parse(imgUrl, true);
 	if (parsedImgUrl.host !== 'image.webservices.ft.com') {
 		return `https://image.webservices.ft.com/v1/images/raw/${encodeURIComponent(imgUrl)}?source=Alphaville&width=${width}&quality=${quality}`;
