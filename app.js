a78fb2654d9c3caed9ae8215d0ee9db189ea44f1
Dragos Digulescu <dragos.digulescu@iquestgroup.com>
2016-11-10 14:49:37 +0200

    Downgrade to silver tier

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ ftwebservice(app, {
 		"purpose": "Frontend rendering application for FT Alphaville articles and Markets Live.",
 		"audience": "public",
 		"primaryUrl": "https://ftalphaville.ft.com",
-		"serviceTier": "gold"
+		"serviceTier": "silver"
 	},
 	goodToGoTest: function() {
 		return true;
