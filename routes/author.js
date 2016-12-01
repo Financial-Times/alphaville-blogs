e91f30187d662c23e4a379a34003a51bd89f35c1
laurent-muchacho <laurent.muchacho@ft.com>
2016-12-01 15:03:11 +0000

    author route refactor to be consistent with new url format

--- routes/author.js
+++ routes/author.js
@@ -4,6 +4,6 @@ const router = require('express').Router();
 const authorCtrl = require('../lib/controllers/authorCtrl');
 
 /* GET search result page. */
-router.get('/', authorCtrl);
+router.get('/:author', authorCtrl);
 
 module.exports = router;
