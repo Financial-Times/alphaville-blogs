932c696b830c251ea884528687e0d23db216f242
laurent-muchacho <laurent.muchacho@ft.com>
2016-12-02 16:17:42 +0000

    Drawer nav refactoring

--- routes/about.js
+++ routes/about.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const router = require('express').Router();
-const aboutCtrl = require('../lib/controllers/wpStaticCtrl')('about', 'about-ft-alphaville');
+const aboutCtrl = require('../lib/controllers/wpStaticCtrl')('about', 'about-ft-alphaville', false, 'About FTAV');
 
 router.use('/', aboutCtrl);
 
--- routes/longroom.js
+++ routes/longroom.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const router = require('express').Router();
-const longroomCtrl = require('../lib/controllers/wpStaticCtrl')('about', 'longroom-under-construction', 'long.room');
+const longroomCtrl = require('../lib/controllers/wpStaticCtrl')('about', 'longroom-under-construction', 'long.room', 'Long Room');
 
 router.use('/', longroomCtrl);
 
