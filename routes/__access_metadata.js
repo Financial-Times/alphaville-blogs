9a84c629f89f34bec120ed7e89bbdbf164573cfe
Dragos Digulescu <dragos.digulescu@iquestgroup.com>
2017-01-13 12:43:27 +0200

    Rollback the last commit

--- routes/__access_metadata.js
+++ routes/__access_metadata.js
@@ -14,11 +14,11 @@ const av2AccessMetadata = [{
 	path_regex: "/\\d{4}/\\d{2}/\\d{2}/\\d{1,}/.+",
 	classification: "conditional_registered"
 }, {
-	path_regex: "/longroom.*",
-	classification: "conditional_registered"
-}, {
 	path_regex: "/longroom/.*",
 	classification: "conditional_alphaville_longroom"
+}, {
+	path_regex: "/longroom.*",
+	classification: "conditional_registered"
 }, {
 	path_regex: ".*",
 	classification: "unconditional"
