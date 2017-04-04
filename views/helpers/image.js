e705075f8235b9c7f8e9eb42ee08dd44858e6fcd
Roland Vachter <roland.vachter@iquestgroup.com>
2017-04-04 14:59:32 +0300

    Use image service v2.

--- views/helpers/image.js
+++ views/helpers/image.js
@@ -10,16 +10,17 @@ module.exports = (imgUrl, width, quality) => {
 
 	const parsedImgUrl = url.parse(imgUrl, true);
 
-	if (parsedImgUrl.host !== 'image.webservices.ft.com') {
-		return `https://image.webservices.ft.com/v1/images/raw/${encodeURIComponent(imgUrl)}?source=Alphaville&width=${width}&quality=${quality}`;
-	}
-	/*
 	// transform image service v1 URLs to v2
 	if (parsedImgUrl.host === 'image.webservices.ft.com') {
-		let pathname = parsedImgUrl.pathname.replace('v1', 'v2');
+		const pathname = parsedImgUrl.pathname.replace('v1', 'v2');
 		return `https://www.ft.com/__origami/service/image${pathname}?source=Alphaville&width=${width}&quality=${quality}`;
-	}*/
-	parsedImgUrl.query = _.extend({}, parsedImgUrl.query, {source:'Alphaville', width, quality});
+	}
+
+	if (!parsedImgUrl.pathname.startsWith('/__origami/service/image')) {
+		return `https://www.ft.com/__origami/service/image/v2/images/raw/${encodeURIComponent(imgUrl)}?source=Alphaville&width=${width}&quality=${quality}`;
+	}
+
+	parsedImgUrl.query = _.extend({}, parsedImgUrl.query, {source: 'Alphaville', width, quality});
 	parsedImgUrl.search = undefined;
 	return url.format(parsedImgUrl);
 };
