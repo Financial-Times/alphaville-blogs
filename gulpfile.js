16075083fafe244c1788658e6ca285ec3d02986f
Roland Vachter <roland.vachter@iquestgroup.com>
2018-06-04 09:41:06 +0300

    Update origami components to their latest version.

--- gulpfile.js
+++ gulpfile.js
@@ -5,55 +5,55 @@ const env = process.env.ENVIRONMENT || 'test';
 
 alphavilleBuildTools(gulp, {
 	env: env,
-	buildFolder: 'public/build',
+	'build-folder': 'public/build',
 	builds: [
 		{
 			id: 'main',
 			standalone: 'mainBundle',
 			js: './assets/js/main.js',
 			sass: './assets/scss/index-page.scss',
-			buildJs: 'main.js',
-			buildCss: 'main.css'
+			'build-js': 'main.js',
+			'build-css': 'main.css'
 		},
 		{
 			id: 'article',
 			standalone: 'articleBundle',
 			js: './assets/js/article.js',
 			sass: './assets/scss/article-page.scss',
-			buildJs: 'article.js',
-			buildCss: 'article.css'
+			'build-js': 'article.js',
+			'build-css': 'article.css'
 		},
 		{
 			id: 'meetTheTeam',
 			standalone: 'meetTheTeam',
 			js: './assets/js/meetTheTeam.js',
 			sass: './assets/scss/meet-the-team.scss',
-			buildJs: 'meetTheTeam.js',
-			buildCss: 'meet-the-team.css'
+			'build-js': 'meetTheTeam.js',
+			'build-css': 'meet-the-team.css'
 		},
 		{
 			id: 'ml-live',
 			standalone: 'mlLiveBundle',
 			js: './assets/js/ml-live.js',
 			sass: './assets/scss/ml-live.scss',
-			buildJs: 'ml-live.js',
-			buildCss: 'ml-live.css'
+			'build-js': 'ml-live.js',
+			'build-css': 'ml-live.css'
 		},
 		{
 			id: 'ml-index',
 			standalone: 'mlIndexBundle',
 			js: './assets/js/ml-index.js',
 			sass: './assets/scss/ml-index.scss',
-			buildJs: 'ml-index.js',
-			buildCss: 'ml-index.css'
+			'build-js': 'ml-index.js',
+			'build-css': 'ml-index.css'
 		},
 		{
 			id: 'curation',
 			standalone: 'mlIndexBundle',
 			js: './assets/js/curation.js',
 			sass: './assets/scss/curation.scss',
-			buildJs: 'curation.js',
-			buildCss: 'curation.css'
+			'build-js': 'curation.js',
+			'build-css': 'curation.css'
 		}
 	]
 });
