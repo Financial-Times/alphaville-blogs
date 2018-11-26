330e434c581f67772b2a5223ebd201c63596af17
Gabor.Ottlik <gabor.ottlik@OttlikG.local>
2018-11-26 14:24:24 +0000

    Delete most popular and commented page

--- routes/mostCommented.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-const router = require('express').Router();
-const mostCommentedCtrl = require('../lib/controllers/mostCommentedCtrl');
-
-router.get('/', mostCommentedCtrl);
-
-module.exports = router;
