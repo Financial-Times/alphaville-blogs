4e0f0c45d68424f7de8529370373d7d4661fccf5
Samuel Parkinson <51677+sjparkinson@users.noreply.github.com>
2019-12-13 12:36:27 +0000

    feat: load markets live article content from content/markets-live/

--- routes/marketslive.js
+++ routes/marketslive.js
@@ -9,7 +9,7 @@ const auth = require('alphaville-auth-middleware');
 router.get('/', marketsliveCtrl.index);
 router.get('/about', marketsliveCtrl.about);
 router.get(/^\/([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})$/, auth(), marketsliveCtrl.byUuid);
-router.get(/^\/([0-9]+\-[0-9]+\-[0-9]+-?[0-9]+?\/?)$/, auth(), marketsliveCtrl.byVanity);
+router.get("/:date([0-9]+\-[0-9]+\-[0-9]+-?[0-9]+?)", auth(), marketsliveCtrl.byVanity);
 
 router.get('/api/sessions/all', marketsLiveApiCtrl.sessions.all);
 router.get('/api/sessions/latest', marketsLiveApiCtrl.sessions.latest);
